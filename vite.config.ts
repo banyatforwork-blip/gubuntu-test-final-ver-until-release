41edbaedb2359403dc9da8bf32b5b884c26b589f
banyatforwork-blip <banyatforwork@gmail.com>
2025-11-12 20:10:37 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
   // 🚨 ADD THIS LINE TO FIX GITHUB PAGES PATHS 🚨
-  base: './',
+  base: '/',
 
   plugins: [
     react(),
